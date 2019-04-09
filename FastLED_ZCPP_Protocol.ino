36de23e0f61cad87b72a7bbf589612b90b4d6f5a
Microcyb <37877848+microcyb@users.noreply.github.com>
2019-04-09 13:52:26 -0500

    Added links to sources

--- FastLED_ZCPP_Protocol.ino
+++ FastLED_ZCPP_Protocol.ino
@@ -19,8 +19,8 @@
 #define FASTLED_SHOW_CORE 0
 #define FASTLED_ESP8266_RAW_PIN_ORDER
 #define FASTLED_ALLOW_INTERRUPTS 0
-#include "ESPAsyncZCPP.h"
-#include <FastLED.h>
+#include "ESPAsyncZCPP.h" //https://github.com/keithsw1111/ESPixelStick
+#include <FastLED.h> //https://github.com/keithsw1111/ESPixelStick
 FASTLED_USING_NAMESPACE
 
 // FASTLED SETUP
